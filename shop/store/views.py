bd00dc3834b2b3455b24e53d5fbba89d01802f90
Tk10 <60160284@go.buu.ac.th>
2021-04-30 17:58:35 +0700

    Add Select filter Homepage #1

--- shop/store/views.py
+++ shop/store/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from store.models import Category, Product,Typefile,Published
 #from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    products = None
+    products=Product.objects.all().filter()
+    return render(request,'index.html',{'products' :products})
 
 def product(request):
    return render(request,'product.html')
